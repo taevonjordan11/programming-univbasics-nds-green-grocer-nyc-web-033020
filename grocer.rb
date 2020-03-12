9679c1643451b55b4ebe99fdb5deebd929957d1f
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-12 21:44:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -73,9 +73,9 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
- checkout = consolidate_cart( cart )
-  checkout = apply_coupons( checkout, coupons )
-  checkout = apply_clearance( checkout )
+ checkout = consolidate_cart(cart)
+  checkout = apply_coupons(checkout, coupons)
+  checkout = apply_clearance(checkout)
 
   index = 0
   grand_total = 0
